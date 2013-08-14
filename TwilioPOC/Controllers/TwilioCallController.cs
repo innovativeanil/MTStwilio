1175e385d823a6d2468b11d9067c47030441ec4a
Scott Vossen <scottvossen@gmail.com>
2013-08-14 12:45:00 -0500

    get controller call working better

--- TwilioPOC/Controllers/TwilioCallController.cs
+++ TwilioPOC/Controllers/TwilioCallController.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Twilio.Mvc;
+using Twilio.TwiML;
 
 namespace TwilioPOC.Controllers
 {
-    public class TwilioCallController : Controller
+    public class TwilioCallController : ApiController
     {
-        //
-        // GET: /TwilioCall/
-
-        public ActionResult Index()
+        [HttpPost]
+        public HttpResponseMessage Post(VoiceRequest request)
         {
-            return View();
-        }
+            var response = new TwilioResponse();
 
+            response.Say("Welcome to this Twilio demo app. Please enter your 5 digit ID.");
+            response.Gather(new { numDigits = 5, action = string.Format("/api/Authenticate") });
+
+            return this.Request.CreateResponse(
+                HttpStatusCode.OK, response.Element, new XmlMediaTypeFormatter());
+        }
     }
 }
