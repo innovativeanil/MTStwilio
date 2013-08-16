e7a8e2d32eb0c5c55a1b4eb2cc51ce2e71e5f8f4
Scott Vossen <scottvossen@gmail.com>
2013-08-16 08:43:19 -0500

    Update Constants

--- TwilioPOC/Constants.cs
+++ TwilioPOC/Constants.cs
@@ -8,8 +8,8 @@ namespace TwilioPOC
     public static class Constants
     {
         public const string ControllerDirectory = "http://twiliopoc.apphb.com/api/";
-        public const string TwilioSid = "AC13f02fa06d1853607c7b7a271a973e17";
-        public const string TwilioToken = "f679f482d39e5b53d0294617d16e85e6";
-        public const string TwilioNumber = "507-489-4021";
+        public const string TwilioSid = "";
+        public const string TwilioToken = "";
+        public const string TwilioNumber = "";
     }
 }
